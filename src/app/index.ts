81c8ce0cad86c8d4eaa3bd528853d93f8b97fa8f
Artem Halishchuk <artem.galishchuk@gmail.com>
2021-10-31 08:56:57 +0200

    '-'

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,4 +1,9 @@
 import {Human} from "./human";
+import {Subject} from "rxjs";//подключили библиотеку rx
 import '../styles/main.css'; //импортим стили
-let human = new Human('vasia', 'vasilev');
-alert('human');
+// let human = new Human('vasia', 'vasilev');
+// alert(human.name);
+let subject = new Subject();
+subject.subscribe(s => alert(s));
+subject.subscribe(s => console.log(s));
+subject.next('hello');
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,13 @@
 const path = require('path'); // подключили пути из ноды
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 module.exports = {
-    entry: path.resolve(__dirname, 'src/app/index.ts'),
+    entry: {
+        app: path.resolve(__dirname, 'src/app/index.ts'), //основной файл для сборщика точка входа
+        test: path.resolve(__dirname, 'src/assets/test_script.js'), //доболнительные файлы для сборки
+    },
     output: { //куда все уйдет
-        filename: 'bundle.js', //все собирется в этот файл
+        filename: '[name].[contenthash].js', //все собирется в этот файл, [contenthash] - переименовует согласно хеша
         path: path.resolve(__dirname, 'dist')
     },
     module: {
@@ -23,9 +27,11 @@ module.exports = {
         extensions: ['*', '.js', '.jsx', '.tsx', '.ts'],
     },
     mode: 'development',
+    //mode: 'production',
     plugins: [
         new HtmlWebpackPlugin( {
             template: path.resolve(__dirname, './src/index.html')
-        })
+        }),
+        new CleanWebpackPlugin()
     ]
 }
